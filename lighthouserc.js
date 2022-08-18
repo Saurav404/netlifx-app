e3fba14b4db3845a6fd6b6799cf26f5a1fecbe0f
unknown <saurab.rawat@jamsfy.com>
2022-08-19 00:03:16 +0530

    cli for lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,10 +1,5 @@
 module.exports = {
   ci: {
-    collect: {
-      /* Add configuration here */
-      staticDistDir: "./public",
-      url: ["http://localhost:3000"],
-    },
     upload: {
       /* Add configuration here */
       target: "temporary-public-storage",
