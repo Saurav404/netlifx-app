64a97e0a53496bba2dc14970fccbbd4c3fe374bd
unknown <saurab.rawat@jamsfy.com>
2022-09-03 16:06:14 +0530

    minor change

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -23,7 +23,7 @@ export default function Login() {
           </span>
           <small>
             This page is protected by Google reCAPTCHA to ensure you are not a
-            boti. <b>Learn more</b>.
+            botii. <b>Learn more</b>.
           </small>
         </form>
       </div>
