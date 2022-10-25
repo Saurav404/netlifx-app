a4198139b93cfc5e7073c4f74fced94d69f4203b
unknown <saurab.rawat@jamsfy.com>
2022-10-25 20:39:38 +0530

    new edit

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -23,7 +23,7 @@ export default function Login() {
           </span>
           <small>
             This page is protected by Google reCAPTCHA to ensure you are not a
-            boty! <b>Learn more</b>.
+            bot! <b>Learn more</b>.
           </small>
         </form>
       </div>
