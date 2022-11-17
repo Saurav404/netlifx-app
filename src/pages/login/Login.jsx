8c95c8e26a8a494ae676d87582c87bebf7e28aac
unknown <saurab.rawat@jamsfy.com>
2022-11-17 11:27:10 +0530

    testing the netlify

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -23,7 +23,7 @@ export default function Login() {
           </span>
           <small>
             This page is protected by Google reCAPTCHA to ensure you are not a
-            bot! <b>Learn more.</b>
+            bot! <b>Learn more!</b>
           </small>
         </form>
       </div>
